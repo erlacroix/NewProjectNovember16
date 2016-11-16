a3c333068a91333416aa1dbb65ab9f80900ae452
Elizabeth <erlcroix@umich.edu>
2016-11-16 09:18:18 -0500

    Practicing committing

--- app/src/main/java/com/example/elizabeth/newprojectnovember16/MainActivity.java
+++ app/src/main/java/com/example/elizabeth/newprojectnovember16/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    //change my code!
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
